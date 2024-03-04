2041d09fe7e3361c9e34eacb0bb8ee8121f14839
Lorenzo <lorenzo.2022310934@aluno.iffar.edu.br>
2024-03-04 17:04:54 -0300

    crud

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tópicos Aula</title>
 </head>
+
 <body>
-<?php
+    <?php
 
-  include("conecta.php");
+    include("conecta.php");
 
-  $sql = "SELECT * FROM usuario";
-  //executa o select
-  $resultado = mysqli_query($conexao, $sql);
+    $sql = "SELECT * FROM usuario";
+    //executa o select
+    $resultado = mysqli_query($conexao, $sql);
 
-  //lista os itens
-  echo ' <div class="table">
-  <table>
+    //lista os itens
+    echo '<table>
     <tr>
     <strong>
       <th scope="col">Id</th>
@@ -26,16 +27,16 @@
     </strong>
     </tr>';
 
-  while ($dados = mysqli_fetch_assoc($resultado)) {
-    echo "<td> " . $dados['id_usuario'] . " </td>";
-    echo "<td> " . $dados['email'] . "</td>";
-    echo "<td><a href='editarForm.php?email=" . $dados['email'] . "&senha=" . $dados['senha'] . "'>" . "<img src='settings.png' widht='20' height='20'" . "</a>";
-    echo " <a href='excluir.php?id_usuario=" . $dados['id_usuario'] . "'>" . "<img src='lixeira.png' widht='20' height='20'" . "</a></td>";
-    echo '</tr>';
-  }
-  echo '</table>
-</div>';
+    while ($dados = mysqli_fetch_assoc($resultado)) {
+        echo "<td> " . $dados['id_usuario'] . " </td>";
+        echo "<td> " . $dados['email'] . "</td>";
+        echo "<td><a href='editarForm.php?email=" . $dados['email'] . "&senha=" . $dados['senha'] . "'>" . "<img src='settings.png' widht='20' height='20'" . "</a>";
+        echo " <a href='excluir.php?id_usuario=" . $dados['id_usuario'] . "'>" . "<img src='lixeira.png' widht='20' height='20'" . "</a></td>";
+        echo '</tr>';
+    }
+    echo '</table>';
 
-  ?>
+    ?>
 </body>
+
 </html>
