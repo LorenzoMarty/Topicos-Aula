d41d6c1bd5662f23b85ea00d5b18d3f31892d82a
lorenzo <lorenzo@gmail.com>
2024-03-11 09:03:22 -0300

    crud

--- cadastrar.php
+++ cadastrar.php
@@ -4,7 +4,7 @@ $senha = $_POST['senha'];
 
 include("conecta.php");
 
-$sql = "INSERT INTO usuario(email, senha) VALUES ($email, $senha)";
+$sql = "INSERT INTO usuario(email, senha) VALUES ('$email', '$senha')";
 
 if (mysqli_query($conexao, $sql)) {
     echo "Arquivo enviado com sucesso!";
--- editar.php
+++ editar.php
@@ -1,2 +1,14 @@
 <?php
+$email = $_GET['email'];
+$senha = $_GET['senha'];
+
+include("conecta.php");
+
 $sql = "UPDATE usuario SET email = '$email', senha = '$senha' WHERE id_usuario= '$idU'";
+
+if (mysqli_query($conexao, $sql)) {
+    echo "Arquivo enviado com sucesso!";
+    header('Location: index.php');
+} else {
+    echo "Falha ao enviar arquivo.";
+}
